f6ad3811fc94925511aee0f79893db5c322ab1fd
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-06-25 02:40:09 +0300

    cleanup makefile logic for portaudio static lib

--- pyx/libportaudio.mk
+++ pyx/libportaudio.mk
@@ -7,19 +7,19 @@ UNAME_S := $(shell uname -s)
 PORTAUDIO := ../pure-data/portaudio/portaudio
 
 PORTAUDIO_CFLAGS = -DNEWBUFFER \
-    -I$(PORTAUDIO)/include \
-    -I$(PORTAUDIO)/src/common
+	-I$(PORTAUDIO)/include \
+	-I$(PORTAUDIO)/src/common
 
 PORTAUDIO_CPPFLAGS += \
-    -I$(PORTAUDIO)/include \
-    -I$(PORTAUDIO)/src/common
+	-I$(PORTAUDIO)/include \
+	-I$(PORTAUDIO)/src/common
 
 PORTAUDIO_LDFLAGS = -lportaudio \
-    -framework CoreAudio \
-    -framework AudioToolbox \
-    -framework AudioUnit \
-    -framework CoreFoundation \
-    -framework CoreServices
+	-framework CoreAudio \
+	-framework AudioToolbox \
+	-framework AudioUnit \
+	-framework CoreFoundation \
+	-framework CoreServices
 
 #########################################
 ##### Files, Binaries, & Libs #####
@@ -27,34 +27,34 @@ PORTAUDIO_LDFLAGS = -lportaudio \
 PORTAUDIO_STATIC_LIB = ../libs/libportaudio.a
 
 PORTAUDIO_SOURCES = \
-    $(PORTAUDIO)/src/common/pa_allocation.c \
-    $(PORTAUDIO)/src/common/pa_converters.c \
-    $(PORTAUDIO)/src/common/pa_cpuload.c \
-    $(PORTAUDIO)/src/common/pa_debugprint.c \
-    $(PORTAUDIO)/src/common/pa_dither.c \
-    $(PORTAUDIO)/src/common/pa_front.c \
-    $(PORTAUDIO)/src/common/pa_process.c \
-    $(PORTAUDIO)/src/common/pa_ringbuffer.c \
-    $(PORTAUDIO)/src/common/pa_stream.c \
-    $(PORTAUDIO)/src/common/pa_trace.c
+	$(PORTAUDIO)/src/common/pa_allocation.c \
+	$(PORTAUDIO)/src/common/pa_converters.c \
+	$(PORTAUDIO)/src/common/pa_cpuload.c \
+	$(PORTAUDIO)/src/common/pa_debugprint.c \
+	$(PORTAUDIO)/src/common/pa_dither.c \
+	$(PORTAUDIO)/src/common/pa_front.c \
+	$(PORTAUDIO)/src/common/pa_process.c \
+	$(PORTAUDIO)/src/common/pa_ringbuffer.c \
+	$(PORTAUDIO)/src/common/pa_stream.c \
+	$(PORTAUDIO)/src/common/pa_trace.c
 
 ifeq ($(UNAME_S),Linux)
 PORTAUDIO_CPPFLAGS += -I$(PORTAUDIO)/src/os/unix
 PORTAUDIO_SOURCES += \
-    $(PORTAUDIO)/src/os/unix/pa_unix_hostapis.c \
-    $(PORTAUDIO)/src/os/unix/pa_unix_util.c \
-    $(PORTAUDIO)/src/hostapi/alsa/pa_linux_alsa.c
+	$(PORTAUDIO)/src/os/unix/pa_unix_hostapis.c \
+	$(PORTAUDIO)/src/os/unix/pa_unix_util.c \
+	$(PORTAUDIO)/src/hostapi/alsa/pa_linux_alsa.c
 endif
 
 ifeq ($(UNAME_S),Darwin)
 PORTAUDIO_CFLAGS += -DPA_USE_COREAUDIO
 PORTAUDIO_CPPFLAGS += -I$(PORTAUDIO)/src/os/unix
 PORTAUDIO_SOURCES += \
-    $(PORTAUDIO)/src/os/unix/pa_unix_hostapis.c \
-    $(PORTAUDIO)/src/os/unix/pa_unix_util.c \
-    $(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core.c \
-    $(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_blocking.c \
-    $(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_utilities.c
+	$(PORTAUDIO)/src/os/unix/pa_unix_hostapis.c \
+	$(PORTAUDIO)/src/os/unix/pa_unix_util.c \
+	$(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core.c \
+	$(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_blocking.c \
+	$(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_utilities.c
 # required for PortAudio to build on newer versions of macOS as it
 # disables deprecation warnings stopping the build
 PORTAUDIO_CFLAGS += -Wno-error -Wno-deprecated
@@ -84,41 +84,50 @@ endif
 # endif
 # endif
 
-
-PORTAUDIO_OBJS = $(patsubst %.c, %.o, $(PORTAUDIO_SOURCES))
-
-# empty var for headers list footer
-empty =
-
-# include the headers in the dist so you can build
-# find portaudio -type file -name *.h | sort | awk '{print "   ", $1, "\\"}'; echo '     $(empty)'
 PORTAUDIO_HEADERS = \
-    $(PORTAUDIO)/include/pa_asio.h \
-    $(PORTAUDIO)/include/pa_jack.h \
-    $(PORTAUDIO)/include/pa_linux_alsa.h \
-    $(PORTAUDIO)/include/pa_mac_core.h \
-    $(PORTAUDIO)/include/pa_win_waveformat.h \
-    $(PORTAUDIO)/include/pa_win_wmme.h \
-    $(PORTAUDIO)/include/portaudio.h \
-    $(PORTAUDIO)/src/common/pa_allocation.h \
-    $(PORTAUDIO)/src/common/pa_converters.h \
-    $(PORTAUDIO)/src/common/pa_cpuload.h \
-    $(PORTAUDIO)/src/common/pa_debugprint.h \
-    $(PORTAUDIO)/src/common/pa_dither.h \
-    $(PORTAUDIO)/src/common/pa_endianness.h \
-    $(PORTAUDIO)/src/common/pa_gitrevision.h \
-    $(PORTAUDIO)/src/common/pa_hostapi.h \
-    $(PORTAUDIO)/src/common/pa_memorybarrier.h \
-    $(PORTAUDIO)/src/common/pa_process.h \
-    $(PORTAUDIO)/src/common/pa_ringbuffer.h \
-    $(PORTAUDIO)/src/common/pa_stream.h \
-    $(PORTAUDIO)/src/common/pa_trace.h \
-    $(PORTAUDIO)/src/common/pa_types.h \
-    $(PORTAUDIO)/src/common/pa_util.h \
-    $(PORTAUDIO)/src/hostapi/asio/iasiothiscallresolver.h \
-    $(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_blocking.h \
-    $(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_internal.h \
-    $(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_utilities.h \
-    $(PORTAUDIO)/src/os/unix/pa_unix_util.h \
-    $(PORTAUDIO)/src/os/win/pa_win_coinitialize.h \
-     $(empty)
+	$(PORTAUDIO)/include/pa_asio.h \
+	$(PORTAUDIO)/include/pa_jack.h \
+	$(PORTAUDIO)/include/pa_linux_alsa.h \
+	$(PORTAUDIO)/include/pa_mac_core.h \
+	$(PORTAUDIO)/include/pa_win_waveformat.h \
+	$(PORTAUDIO)/include/pa_win_wmme.h \
+	$(PORTAUDIO)/include/portaudio.h \
+	$(PORTAUDIO)/src/common/pa_allocation.h \
+	$(PORTAUDIO)/src/common/pa_converters.h \
+	$(PORTAUDIO)/src/common/pa_cpuload.h \
+	$(PORTAUDIO)/src/common/pa_debugprint.h \
+	$(PORTAUDIO)/src/common/pa_dither.h \
+	$(PORTAUDIO)/src/common/pa_endianness.h \
+	$(PORTAUDIO)/src/common/pa_gitrevision.h \
+	$(PORTAUDIO)/src/common/pa_hostapi.h \
+	$(PORTAUDIO)/src/common/pa_memorybarrier.h \
+	$(PORTAUDIO)/src/common/pa_process.h \
+	$(PORTAUDIO)/src/common/pa_ringbuffer.h \
+	$(PORTAUDIO)/src/common/pa_stream.h \
+	$(PORTAUDIO)/src/common/pa_trace.h \
+	$(PORTAUDIO)/src/common/pa_types.h \
+	$(PORTAUDIO)/src/common/pa_util.h \
+	$(PORTAUDIO)/src/hostapi/asio/iasiothiscallresolver.h \
+	$(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_blocking.h \
+	$(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_internal.h \
+	$(PORTAUDIO)/src/hostapi/coreaudio/pa_mac_core_utilities.h \
+	$(PORTAUDIO)/src/os/unix/pa_unix_util.h \
+	$(PORTAUDIO)/src/os/win/pa_win_coinitialize.h
+
+
+PORTAUDIO_OBJS := $(PORTAUDIO_SOURCES:%.c=%.o)
+
+
+%.o : %.c
+	@echo "COMPILING SOURCE $< INTO OBJECT $@"
+	@$(CC) -c $(PORTAUDIO_CFLAGS) $< -o $@
+
+$(PORTAUDIO_STATIC_LIB): $(PORTAUDIO_OBJS)
+	@ar rcs $(PORTAUDIO_STATIC_LIB) $(PORTAUDIO_OBJS)
+
+portaudio: $(PORTAUDIO_STATIC_LIB)
+
+clean-portaudio:
+	@rm -f $(PORTAUDIO_OBJS)
+	@rm -f $(PORTAUDIO_STATIC_LIB)
+
